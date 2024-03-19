784484db0449730303090f66b908a46103983d18
Chung Fan Leung <chuleung@c2r2s9.42london.com>
2024-03-19 23:35:02 +0000

    yo

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:16:15 by chuleung          #+#    #+#             */
-/*   Updated: 2024/03/15 12:00:28 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/03/19 22:35:54 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,39 +19,67 @@
 # include <fcntl.h>
 # include <X11/keysym.h>
 
+typedef struct s_coord_set
+{
+	t_coord	open;
+	t_coord	close;
+	t_coord	current;
+} t_coord_set;
+
+typedef struct s_coord
+{
+	double	x;
+	double	y;
+	double	z;
+	double	RGB;
+	double	x_max;
+	double	y_max;
+} t_coord;
+
+typedef struct s_brese_vars
+{
+	double	delta_x;
+	double	delta_y;
+	double	init_decis_para;
+	double	prev_decis_para;
+	double	decis_para_in_E;
+	double	decis_para_in_NE;
+} t_brese_vars;
+
+typedef enum e_pix_decis
+{
+	E,
+	NE,
+}   t_pix_decis;
+
+
 typedef enum e_mode
 {
-    With_Color,
-    Without_Color,
+	With_Color,
+	Without_Color,
 }   t_mode;
-
-typedef enum e_mode2
-{
-    With_Color,
-    Without_Color,
-}   t_mode2;
 
 typedef struct s_int_strs
 {
-    int     **rgb_strs;
-    int     **values_strs;
-    int     wid;
+	int     **rgb_strs;
+	int     **values_strs;
+	int     wid;
 }   t_int_strs;
 
 
 typedef struct s_mlx_data
 {
-    void *mlx_ptr;
-    void *win_ptr;
+	void *mlx_ptr;
+	void *win_ptr;
 } t_mlx_data;
 
 typedef struct s_img
 {
-    void    *img_ptr;
-    char    *img_pixels_ptr;
-    int     bit_per_pixel;
-    int     endian;
-    int     line_len;
+	void    *img_ptr;
+	char    *img_pixels_ptr;
+	int     bit_per_pixel;
+	int     endian;
+	int     line_len;
 }   t_img;
 
 typedef struct	s_var
@@ -62,17 +90,92 @@ typedef struct	s_var
 }				t_var;
 
 
-typedef struct s_4x4_mtx
-{
-    float mtx[4][4];
-} t_4x4_mtx;
 
+//input_mgt
+void input_mgt(t_int_strs *strs,char **all_lines, int wid);
+
+//input_mgt_val
+int	**get_values_strs(char **all_lines, t_mode mode, int width);
+void creation_process(int **ptrs_to_values_str, char **strs_before_atoi, int len);
+
+//input_mgt_val_w_col
+int	*create_values_str_w_col(char *all_lines);
+
+//input_mgt_rgb
+int	**get_RGB_strs(char **all_lines, int width);
+
+//utilic
 void free_all(char **strs);
+void free_all_int(int **strs);
+void free_stru(t_int_strs *struc);
 
+//input_mgt_rgb2
+void creation_process_rgb(int **ptrs_to_values_str, 
+	char **strs_before_atoi, int len);
+int whether_colors(char *char_strs);
 void	fill_white_for_zero(char **colors_c_arr);
+int find_comma(char *str);
+
+//ft_atoi_base
+int	ft_atoi_base(char *str, char *base);
+
+//bresenham
+int bresenham(t_coord *open, t_coord *close);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+t_int_strs *input_mgt(char **all_lines, int wid);
+
+int	ft_atoi_base(char *str, char *base);
+
+int whether_colors(char *char_strs);
+
+void    free_all(char **strs);
+
+void    fill_white_for_zero(char **colors_c_arr);
+
+int find_comma(char *str);
 
 void	extract_RGB(char *char_str, char **colors_c_arr);
 
 int	check_str_len_for_height(char *char_str);
+
+int	**get_values_strs(char **all_lines, t_mode mode, int width);
+
+int	*create_values_str_w_col(char *all_lines);
+
+int	**get_RGB_strs(char **all_lines, int width);
+
+void creation_process(int **ptrs_to_values_str, 
+	char **strs_before_atoi, int len);
+*/
 
 #endif
--- lib/libft/src/ft_split.c
+++ lib/libft/src/ft_split.c
@@ -3,16 +3,172 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chuleung <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 18:12:10 by chuleung          #+#    #+#             */
-/*   Updated: 2023/11/13 19:38:25 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/03/18 22:50:12 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //#include <stddef.h>
 //#include <stdlib.h>
+
+static size_t	get_str_count(char const *s, char d);
+static char		*extract_str(char const *s, char c);
+
+/**
+ * @brief Splits a string into an array of strings (words) using a specified
+ * delimiter character.
+ *
+ * @param s The string to be split.
+ * @param c The delimiter character.
+ * @return 
+ * A dynamically allocated array of strings. Each string is a word from the
+ * original string 's'. The array is terminated by a NULL pointer. If the
+ * allocation fails, the function returns NULL.
+ * 
+ * The memory for the array and for each word is dynamically allocated and must
+ * be freed by the caller.*/
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	total;
+	char	**strs;
+
+	total = get_str_count(s, c);
+	strs = (char **)malloc((total + 1) * sizeof(char *));
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i])
+		{
+			strs[j] = extract_str(&s[i], c);
+			j++;
+		}
+		while (s[i] != c && s[i])
+			i++;
+	}
+	strs[j] = NULL;
+	return (strs);
+}
+
+static size_t	get_str_count(char const *s, char d)
+{
+	size_t	i;
+	size_t	sum;
+
+	i = 0;
+	sum = 0;
+	while (s[i])
+	{
+		while (s[i] == d)
+			i++;
+		if (s[i] != d && s[i])
+			sum++;
+		while (s[i] != d && s[i])
+			i++;
+	}
+	ft_printf("LH's split%d\n", sum);
+	return (sum);
+}
+
+static char	*extract_str(char const *s, char c)
+{
+	char	*out_str;
+	size_t	len;
+
+	len = 0;
+	while (s[len] != c && s[len])
+		len++;
+	out_str = (char *)malloc((len + 1) * sizeof(char));
+	ft_memcpy(out_str, s, len);
+	out_str[len] = '\0';
+	return (out_str);
+}
+
+
+
+/*
+Simon!!!!!!!!!!!!!!!!!!!
+static size_t	ft_count_word(const char *s, char c)
+{
+	size_t	res;
+
+	res = 0;
+	while (s && *s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			while (*s && *s != c)
+				s++;
+			res++;
+		}
+	}
+	return (res);
+}
+
+static size_t	ft_word_length(const char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s && s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static char	**ft_clean_up(size_t k, char **arr)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < k)
+	{
+		free(arr[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char		**res;
+	size_t		i;
+	size_t		k;
+
+	k = 0;
+	res = malloc(sizeof(char *) * (ft_count_word(s, c) + 1));
+	while (res && s && *s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+		{
+			res[k] = malloc(sizeof(char) * (ft_word_length(s, c) + 1));
+			if (!res[k])
+				return (ft_clean_up(k, res));
+			i = -1;
+			while (s[++i] && s[i] != c)
+				res[k][i] = s[i];
+			res[k++][i] = 0;
+			s += i;
+		}
+	}
+	if (res)
+		res[k] = NULL;
+	return (res);
+}
+*/
+
+/*
+mine!!!!!!!!!!!!!!!!!!!
 
 static size_t	nr_of_arrays(char const *s, char d)
 {
@@ -26,9 +182,11 @@ static size_t	nr_of_arrays(char const *s, char d)
 		while (s[i] == d)
 			i++;
 		if (s[i] != d && s[i])
+		{
+			while (s[i] != d && s[i])
+				i++;
 			sum++;
-		while (s[i] != d && s[i])
-			i++;
+		}
 	}
 	return (sum);
 }
@@ -63,6 +221,7 @@ char	**ft_split(char const *s, char c)
 	i = 0;
 	j = 0;
 	total = nr_of_arrays(s, c);
+	ft_printf("My_split%d\n", total);
 	strs = (char **)malloc(sizeof(char *) * (total + 1));
 	while (s[i])
 	{
@@ -79,6 +238,7 @@ char	**ft_split(char const *s, char c)
 	strs[j] = NULL;
 	return (strs);
 }
+*/
 
 /*#include <stdio.h>
 
