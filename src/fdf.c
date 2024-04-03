2fbc488dd08ca5cc6cb12e8f5d6263b21609ca10
Chung Fan Leung <chuleung@c3r1s4.42london.com>
2024-04-03 19:24:20 +0100

    add

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 14:19:03 by chuleung          #+#    #+#             */
-/*   Updated: 2024/04/03 17:00:20 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/04/03 19:23:45 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,9 @@ int		main(int ac, char **av)
 	vertex_arr = vertex_create(&all_strs, read_vars.wid, &vars);
 	populate_vertexes_in_map(&vars, vertex_arr); //scale the verterice with coord that fits into the map
 	vars.vertex_arr = vertex_arr;
+	while(vertex_arr[1] < vars
+
+/*
 	window_handle(&vars);
 	transform_all_vertexes(&vars, mx_iso4x4()); //iso matrix * the coord of each vertex;
 	ortho_model(&vars);
@@ -56,6 +59,7 @@ int		main(int ac, char **av)
 	window_close(&vars, vertex_arr, &all_strs, read_vars.fd);
 	return (0);
 }
+*/
 
 /*
 int	main(int ac, char **av)
@@ -92,3 +96,58 @@ int	main(int ac, char **av)
 	return (0);
 }
 */
+
+/*
+int	main(int ac, char **av)
+{
+	int			fd;
+	int			wid;
+	char		**all_lines;
+	t_int_strs	all_strs;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	fd = 0;
+	wid = 0;
+	all_lines = NULL;
+	if (ac != 2 || ((fd = read_file(av[1])) == -1))
+		return (1);
+	wid = find_wid(fd);
+	close(fd);
+	if ((fd = read_file(av[1])) == -1)
+    	return (1);
+	all_lines = extract_line(fd, wid);
+	input_mgt(&all_strs, all_lines, wid);
+	while (all_strs.values_strs[i])
+	{
+		j = 0;
+		while (j < all_strs.values_strs[i][0] + 1)
+		{
+			ft_printf("%d,", all_strs.values_strs[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+	ft_printf("\n\n");
+	if (all_strs.rgb_strs != NULL)
+	{
+		i = 0;
+		while (all_strs.rgb_strs[i])
+		{
+			j = 0;
+			while (j < all_strs.rgb_strs[i][0] + 1)
+			{
+				ft_printf("%x,", all_strs.rgb_strs[i][j]);
+				j++;
+			}
+			ft_printf("\n");
+			i++;
+		}
+	}
+	free_stru(&all_strs);
+	close(fd);
+	return (0);
+}
